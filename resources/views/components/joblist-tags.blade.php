25136a6639109936c912d373bbe7eb8ea05f90b1
matiasmartin11 <matias.martin.contacto@gmail.com>
2024-01-28 20:10:35 +0100

    fix: responsive tags

--- resources/views/components/joblist-tags.blade.php
+++ resources/views/components/joblist-tags.blade.php
@@ -3,10 +3,10 @@
 @php
 $tags = explode(',', $tagsCsv)
 @endphp
-<ul class="flex mb-4">
+<ul class="flex flex-wrap mb-4">
     @foreach ($tags as $tagname)
-        <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-        <a href="/?tag={{$tagname}}">{{$tagname}}</a>
+        <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 mb-2 text-xs">
+            <a href="/?tag={{ $tagname }}">{{ $tagname }}</a>
         </li>
     @endforeach
 </ul>
