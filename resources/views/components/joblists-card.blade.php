58969181b4bcba6b1afbcf4e034df35d0537943c
matiasmartin11 <matias.martin.contacto@gmail.com>
2024-01-28 20:13:26 +0100

    fix: job offer card is now responsive

--- resources/views/components/joblists-card.blade.php
+++ resources/views/components/joblists-card.blade.php
@@ -1,23 +1,22 @@
-
-
-
 @props(['joblisting'])
-    <div class="flex flex-col items-center justify-center md:flex-row md:justify-start">
-        <img class="flex flex-col w-48 mr-6 md:block"
-            src="{{ $joblisting->logo ? $joblisting->logo : asset('images/no-image.png') }}" alt="" />
-        <div>
-            <h3 class="text-xl mb-4">
-                <a class= "font-bold hover:text-gray-500" href="/joblisting/{{ $joblisting->id }}">{{ $joblisting->title }}</a>
-            </h3>
-            <div class="text-lg mb-4">
-                <i class="fa-solid fa-building"></i>
-                {{ $joblisting->company }}
-            </div>
-            <x-joblist-tags :tagsCsv="$joblisting->tags"/>
-            <div class="text-lg">
-                <i class="fa-solid fa-location-dot"></i>
-                {{ $joblisting->location }}
-            </div>
+
+<div class="flex flex-col items-center justify-center md:flex-row md:justify-start md:items-start">
+    <img class="w-48 mb-4 md:mb-0 md:w-56 mr-6"
+        src="{{ $joblisting->logo ? $joblisting->logo : asset('images/no-image.png') }}" alt="" />
+
+    <div>
+        <h3 class="text-xl mb-4">
+            <a class="font-bold hover:text-gray-500" href="/joblisting/{{ $joblisting->id }}">{{ $joblisting->title }}</a>
+        </h3>
+        <div class="text-lg mb-4">
+            <i class="fa-solid fa-building"></i>
+            {{ $joblisting->company }}
         </div>
-        {{$slot}}
+        <x-joblist-tags :tagsCsv="$joblisting->tags"/>
+        <div class="text-lg">
+            <i class="fa-solid fa-location-dot"></i>
+            {{ $joblisting->location }}
+        </div>
     </div>
+    {{$slot}}
+</div>
